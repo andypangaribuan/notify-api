2fc2b00a651c31adcbd8ac574a39d553751af088
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2026-07-11 05:49:17 +0700

    update

--- src/db/repo.rs
+++ src/db/repo.rs
@@ -1,12 +1,10 @@
 /*
  * Copyright (c) 2026.
  * Created by Andy Pangaribuan (andypangaribuan@treasury.id)
- *
+ * 
  * This product is protected by copyright and distributed under
  * licenses restricting copying, distribution and decompilation.
  * All Rights Reserved.
  */
 
-mod email_rate_limit;
-
-pub use email_rate_limit::EmailRateLimit;
+pub mod email_rate_limit;
