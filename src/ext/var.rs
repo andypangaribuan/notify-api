f585d1ff2e6a1a7bc1fc4d66c244d8132fe30a93
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2026-07-11 05:59:45 +0700

    update

--- src/ext/var.rs
+++ src/ext/var.rs
@@ -1,9 +1,10 @@
 /*
  * Copyright (c) 2026.
  * Created by Andy Pangaribuan (andypangaribuan@treasury.id)
- * 
+ *
  * This product is protected by copyright and distributed under
  * licenses restricting copying, distribution and decompilation.
  * All Rights Reserved.
  */
 
+pub const DB_NOTIFY: &str = "notify-db";
