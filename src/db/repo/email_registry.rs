84c6096d66856cfaa01a845fa39111106b02c5de
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2026-07-13 05:11:43 +0700

    update

---
+++ src/db/repo/email_registry.rs
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2026.
+ * Created by Andy Pangaribuan (andypangaribuan@treasury.id)
+ *
+ * This product is protected by copyright and distributed under
+ * licenses restricting copying, distribution and decompilation.
+ * All Rights Reserved.
+ */
+
+use crate::{db::entity::EmailRegistry, ext::var::DB_NOTIFY};
+use rmod::db::{self, Error, PgArgs, Repo};
+
+const TABLE_NAME: &str = "email_registry";
+const REPO: Repo<EmailRegistry> = Repo::new(TABLE_NAME, "created_at, updated_at, deleted_at, uid, domain_name, sender_email, email_conf");
+
+pub async fn fetch(where_clause: &str, args: PgArgs<EmailRegistry>) -> Result<Option<EmailRegistry>, Error> {
+    REPO.fetch_on(DB_NOTIFY, where_clause, args).await
+}
